271360dff16f42f44e50ade9155b542d69427e06
Luis Gaytan <gaytan_luis@msn.com>
2016-06-16 17:38:07 -0700

    basic layout complete, with parallax

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//    $("a").hover(function() {
+//       console.log($(this).id);
+//       $(this).find("img").attr("src","./assets/images/LinkedIn-hover.png");},
+//       function() {
+//         $(this).find("img").attr("src","./assets/images/"+this.id+".png");
+// 		});
+// });
