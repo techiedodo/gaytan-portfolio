1366258df3c102667c5c409c97671cfc88a8b05f
Luis Gaytan <gaytan_luis@msn.com>
2016-06-17 00:12:04 -0700

    working; issue when width is under 1025--leaves a gap on the right side of the screen. Will attempt to fix

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,26 @@
 //         $(this).find("img").attr("src","./assets/images/"+this.id+".png");
 // 		});
 // });
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+    $('.hero-content').css({
+      'transform' : 'translateX('+ wScroll /6+'%)'
+    });
+  });
+
+  var message = "";
+
+  $("#sendMessage").on("click", function() {
+    message = $("#contactForm").serializeArray();
+    $.ajax({
+        url: "//formspree.io/agaytan@techiedodo.com",
+        method: "POST",
+        data: {message: message},
+        dataType: "json"
+    });
+    alert('Thanks for the email, we\'ll be in touch promptly.');
+    $("#contactForm")[0].reset();
+    return false;
+  });
+});
