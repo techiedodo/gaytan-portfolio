4222959a234132630ce82a43e6661f2f6b31f9b0
Luis Gaytan <gaytan_luis@msn.com>
2016-08-18 23:38:16 -0700

    study section with show/hide and fade

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,57 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    var wScroll = $(this).scrollTop();
-    $('.hero-content').css({
-      'transform' : 'translateX('+ wScroll /6+'%)'
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var wScroll = $(this).scrollTop();
+        $('.hero-content').css({
+            'transform': 'translateX(' + wScroll / 6 + '%)'
+        });
+
+        if (wScroll > $('.top-row').offset().top - ($(window).height() / 2.5)) {
+            $('.services-section p').each(function(i) {
+                setTimeout(function() {
+                    $('.services-section p').eq(i).addClass('is-showing');
+                }, 550 * (i + 1));
+            });
+        };
     });
 
-    if(wScroll > $('.top-row').offset().top - ($(window).height() / 2.5) ) {
-      $('.services-section p').each(function(i){
-        setTimeout(function(){
-          $('.services-section p').eq(i).addClass('is-showing');
-        }, 550 * (i+1));
-      });
-    };
-  });
-
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 500);
-          return false;
-        }
-      }
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                }
+            }
+        });
     });
-  });
 
+    $('#study-section').hide();
 
-
-  var message = "";
-
-  $("#sendMessage").on("click", function() {
-    message = $("#contactForm").serializeArray();
-    $.ajax({
-        url: "//formspree.io/agaytan@techiedodo.com",
-        method: "POST",
-        data: {message: message},
-        dataType: "json"
+    $('.show_hide').click(function(e) {
+        $('#study-section').slideToggle(300, function() {
+            $('.study-sections').css('opacity', '1');
+        });
+        e.preventDefault();
     });
-    alert('Thanks for the email, we\'ll be in touch promptly.');
-    $("#contactForm")[0].reset();
-    return false;
-  });
+
+    var message = "";
+
+    $("#sendMessage").on("click", function() {
+        message = $("#contactForm").serializeArray();
+        $.ajax({
+            url: "//formspree.io/agaytan@techiedodo.com",
+            method: "POST",
+            data: {
+                message: message
+            },
+            dataType: "json"
+        });
+        alert('Thanks for the email, we\'ll be in touch promptly.');
+        $("#contactForm")[0].reset();
+        return false;
+    });
 });
